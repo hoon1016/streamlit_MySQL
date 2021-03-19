d92f43d22f9d4d082796401703468c0ad7d2b6a9
hoon1016 <wjsrms76@gmail.com>
2021-03-19 12:13:04 +0900

    Python으로 MySQL 접속

--- app.py
+++ app.py
@@ -1,67 +1,104 @@
 import streamlit as st
 import mysql.connector
 from mysql.connector import Error
-from datetime import datetime,date,time
+from datetime import datetime, date, time
 
 
 def main():
+    # title = st.text_input('책 제목 입력')
+    # author_fname = st.text_input('이름 입력')
+    # author_lname = st.text_input('성 입력')
+    # released_year = st.number_input('년도 입력')
+    # stock_quantity = st.number_input('수량 입력')
+    # pages = st.number_input('페이지입력')
 
-    # name = st.text_input('이름을 입력하세요')
+    # name = st.text_input('이름 입력')
     # birth_date = st.date_input('생년월일')
     # birth_time = st.time_input('시간입력')
-    # date_time = datetime.combine(birth_date,birth_time)
+    
+    # birth_dt = datetime.combine(birth_date, birth_time)
+    
     # print(birth_date)
     # print(birth_time)
-    # print(date_time) 
+    # print(birth_dt)
+
+    st.subheader('몇년도 이후, 몇페이지 이상되는 책을 검색하고 싶으십니까?')
+    released_year = st.number_input('년도 입력', min_value=1800, max_value=2050)
+    pages = st.number_input('페이지수 입력', min_value=10)
+
+    order = 'asc'
+    if st.checkbox("오름차순 / 내림차순") :
+        order = 'desc'
     
-    if st.button('저장하기'):
-    
+    if st.button('조회') :
+
         try :
-            # 1. 커넥터로부터 커넥션을 받는다.
+            # 1. 커넉터로부터 커넥션을 받는다.
             connection = mysql.connector.connect(
                 host = 'database-1.c8incbwwf4p9.us-east-2.rds.amazonaws.com',
                 database = 'yhbe',
-                user ='streamlit',
+                user = 'streamlit',
                 password = 'djaak1234'
-            ) 
-
+            )
+            
             if connection.is_connected() :
-                print('연결됬을때')
-                be_info = connection.get_server_info()
-                print("MySQL servar version : ",be_info)
+                db_info = connection.get_server_info()
+                print("MySQL server version : ", db_info)
 
-                #2.커서를 가져온다.
+                # 2. 커서를 가져온다.
                 cursor = connection.cursor()
 
-                print('커서 가져온것 후')
-                #3. 우리가 원하는거 실행 가능
-            
-                query = '''insert into cats4 (name,age)
-                            values (%s,%s);'''
-            
-                record = [('냐옹이',1),('나비',3),('또기',5)]
-                print(datetime.now())
+                # 3. 우리가 원하는거 실행 가능.            
+                query = """select * from books;"""
                 
-                cursor.executemany(query,record)
-                connection.commit()
-            
-                print("{}개 적용됨".format(cursor.rowcount))
-           
-                #4. 실행 후 커서에서 결과를 빼낸다.
-                # record = cursor.fetchone()
-                # print('Connected to be : ', record)
-    
+                cursor.execute(query)
+
+                
+                # 4. 실행 후 커서에서 결과를 빼낸다. 
+                
+                results = cursor.fetchall()
+
+                
+                print(results)
+
+                # for data in results :
+                #     print(data[1] , data[4])
+
+                
+                cursor = connection.cursor(dictionary= True)
+
+                if order == 'asc' :
+                    query = """ select title, released_year, pages
+                            from books
+                            where released_year > %s and pages > %s
+                            order by released_year asc ; """
+                else :
+                    query = """ select title, released_year, pages
+                            from books
+                            where released_year > %s and pages > %s
+                            order by released_year desc ; """
+                         
+                param = (released_year, pages)
+
+                cursor.execute(query, param)
+                results = cursor.fetchall()
+                
+                for data in results :
+                    print(data['title'], data['released_year'])
+                    st.write(data)
+
+        
         except Error as e :
-            print('디비 관련 에러 발생',e)
+            print('디비 관련 에러 발생', e)
+        
         finally :
-            #5. 모든 데이터 베이스 실행 명령을 전부 끝냈으며,
-            #       커서와 커넥션을 모두 닫아준다.
+            # 5. 모든 데이터베이스 실행 명령을 전부 끝냈으면,
+            #    커서와 커넥션을 모두 닫아준다.
             cursor.close()
             connection.close()
             print("MySQL 커넥션 종료")
 
-
-
-
-if __name__ =='__main__':
+if __name__ == '__main__' :    
     main()
+
+
---
+++ app2.py
@@ -0,0 +1,93 @@
+import streamlit as st
+import mysql.connector
+from mysql.connector import Error
+
+
+def main():
+    
+    book_id_list = []
+    
+    try :
+        # 1. 커넉터로부터 커넥션을 받는다.
+        connection = mysql.connector.connect(
+        host = 'database-1.c8incbwwf4p9.us-east-2.rds.amazonaws.com',
+        database = 'yhbe',
+        user = 'streamlit',
+        password = 'djaak1234'
+    )
+            
+        if connection.is_connected() :
+            cursor = connection.cursor(dictionary= True)        
+        
+            query = """ select *
+                        from books limit 5;"""
+            cursor.execute(query)
+
+            results = cursor.fetchall()
+
+            
+
+            for row in results:
+                st.write(row)
+                book_id_list.append(row['book_id'])
+
+    except Error as e :
+        print('디비 관련 에러 발생', e)
+        
+    finally :
+        cursor.close()
+        connection.close()
+        print("MySQL 커넥션 종료")
+    
+
+    
+    book_id = st.number_input('책 아이디 입력',min_value=book_id_list[0],max_value=book_id_list[-1])
+    
+    stock_quantity = st.number_input('수량 입력',min_value=0)
+
+    pages = st.number_input('페이지 입력',min_value=1)
+    
+    
+    if st.button('실행'):
+        
+        try :
+            # 1. 커넉터로부터 커넥션을 받는다.
+            connection = mysql.connector.connect(
+                host = 'database-1.c8incbwwf4p9.us-east-2.rds.amazonaws.com',
+                database = 'yhbe',
+                user = 'streamlit',
+                password = 'djaak1234'
+            )
+            
+            if connection.is_connected() :
+                            
+                # 2. 커서를 가져온다.
+                cursor = connection.cursor()           
+                query =  '''update books
+                            set pages = %s, stock_quantity = %s
+                            where book_id = 1;'''
+
+                pages = 201
+                stock_quantity = 51
+                book_id =2        
+                data = (pages,stock_quantity)
+
+                cursor.execute(query,data)
+
+                connection.commit()
+
+
+
+        except Error as e :
+            print('디비 관련 에러 발생', e)
+        
+        finally :
+            # 5. 모든 데이터베이스 실행 명령을 전부 끝냈으면,
+            #    커서와 커넥션을 모두 닫아준다.
+            cursor.close()
+            connection.close()
+            print("MySQL 커넥션 종료")
+
+
+if __name__ =='__main__':
+    main()

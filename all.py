64ae695fd6ee1d6ff48cdb96a2580ee6febbbf5d
hoon1016 <wjsrms76@gmail.com>
2021-03-20 23:23:03 +0900

    Update all.py

--- all.py
+++ all.py
@@ -6,11 +6,11 @@ from my_sql_select import my_sql_select
 from insert import run_insert
 from Update import run_Update
 from Delete import run_Delete
-
+from my_sql_pyplot import run_my_sql_pyplot
 
 def main() :
 
-    menu = ['Select', 'Insert', 'Update', 'Delete']
+    menu = ['Select', 'Insert', 'Update', 'Delete','Pyplot']
     choie = st.sidebar.selectbox('메뉴', menu)
 
     if choie == 'Select' :
@@ -21,7 +21,8 @@ def main() :
         run_Update()
     elif choie == 'Delete':
         run_Delete()
-
+    elif choie == 'Pyplot':
+        run_my_sql_pyplot()
 
 if __name__ == '__main__' :
     main()
